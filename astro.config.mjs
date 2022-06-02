a57bf2bf6026360d4e5c215487af30bfd85b9246
Ernesto <feedsnegociobitcoin@gmx.es>
2022-06-02 10:43:13 +0000

    instalacion de tailwind solid sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,10 @@
 import { defineConfig } from 'astro/config';
 
+import solid from "@astrojs/solid-js";
+import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [solid(), tailwind(), sitemap()]
+});
